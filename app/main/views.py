3d672207245ae4384aa4b7f5a4cadc773a15b952
lesliebinbin <506120340@qq.com>
2018-08-22 19:40:55 +1000

    fix the email problem

--- app/main/views.py
+++ app/main/views.py
@@ -4,25 +4,8 @@ from . import main
 from .forms import NameForm
 from .. import db, mail
 from ..models import User
-from flask_mail import Message
-from threading import Thread
-
-
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
-
-
-def send_mail(to, subject, template, **kwargs):
-    msg = Message(
-        main.config['MAIL_SUBJECT_PREFIX'] + subject,
-        sender=main.config['FLASKY_MAIL_SENDER'],
-        recipients=to)
-    msg.body = render_template(template + '.txt', **kwargs)
-    msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[main, msg])
-    thr.start()
-    return thr
+from ..email import send_email
+from flask import current_app
 
 
 @main.route("/", methods=['GET', 'POST'])
@@ -36,6 +19,21 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if current_app._get_current_object().config['FLASKY_ADMIN']:
+                send_email(
+                    [current_app._get_current_object().config['FLASKY_ADMIN']],
+                    'New user',
+                    'mail/new_user',
+                    user=user)
+            else:
+                send_email(
+                    [
+                        '506120340@qq.com',
+                        'lesliebinbin19900129@gmail.com',
+                    ],
+                    'New user',
+                    'mail/new_user',
+                    user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
